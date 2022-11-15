5440f3ebd679c27e1e8400e38ae5474d330d0df7
Petrik Diak <diak@petrik.hu>
2022-11-15 08:47:47 +0100

    1 feladat

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import './style.css';
 
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+    let eredmeny;
+    async function adatBetoltes(){
+        let response = await fetch('/quotes.json');
+        eredmeny = await response.json();
+    }
+
+    adatBetoltes();
+
+    function mindBetolt(quotesLista){
+        let szulo = document.getElementById("all");
+
+        quotesLista = quotesLista.sort((a, b) => a.author.localeCompare(b.author));
+
+        for(let e of quotesLista){
+            let li = document.createElement("li");
+            li.innerHTML = e.author + ": " + e.quote;
+
+            szulo.appendChild(li);
+        }
+    }
+
+    document.getElementById("megjelenit").addEventListener("click", ()=>{
+        mindBetolt(eredmeny.quotes);
+    })
+
+})
+
+
